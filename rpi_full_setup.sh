a1d28211f63a877ea7d03aaad4310a0ac250aae9
Ziye Li <144114689+Liziye725@users.noreply.github.com>
2025-06-20 12:19:06 +0200

    remove shell quote

--- rpi_full_setup.sh
+++ rpi_full_setup.sh
@@ -57,7 +57,7 @@ sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key | 
 print_info "Setting up ROS 2 source (Ubuntu 22.04 only)"
 sudo add-apt-repository universe -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu \$(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt update
 sudo apt install -y ros-humble-ros-base ros-dev-tools python3-rosdep python3-colcon-common-extensions
 
